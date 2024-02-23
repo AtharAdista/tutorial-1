8b3f369fc90a1f6ffc47016cddde7cd5bd412ae0
Athar Adista <athar0165@gmail.com>
2024-02-23 08:32:49 +0700

    fix CarServiceImpl (add Service)

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+@Service
 public class CarServiceImpl implements CarService{
     @Autowired
     private CarRepository carRepository;
