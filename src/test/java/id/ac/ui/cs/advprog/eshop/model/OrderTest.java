b45f36222418b5eb4423bb387419bd0a3c6852f0
Athar Adista <athar0165@gmail.com>
2024-02-29 14:13:42 +0700

    [REFACTOR] Apply OrderStatus enum to Order model tests.

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
@@ -56,8 +57,8 @@ public class OrderTest {
     @Test
     void testCreateOrderSuccessStatus(){
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1708560000L, "Safira Sudrajat", "SUCCESS");
-        assertEquals("SUCCESS", order.getStatus());
+                this.products, 1708560000L, "Safira Sudrajat", OrderStatus.SUCCESS.getValue());
+        assertEquals(OrderStatus.SUCCESS.getValue(), order.getStatus());
     }
 
     @Test
