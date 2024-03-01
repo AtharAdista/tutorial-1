002a63716f9c718b2d72dac19b892be36576b67c
Athar Adista <athar0165@gmail.com>
2024-03-01 10:37:31 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class OrderServiceImpl implements OrderService{
@@ -14,22 +15,33 @@ public class OrderServiceImpl implements OrderService{
 
     @Override
     public Order createOrder(Order order){
+        if (orderRepository.findById(order.getId()) == null){
+            orderRepository.save(order);
+            return order;
+        }
         return null;
     }
 
     @Override
     public Order updateStatus(String orderId, String status){
-        return null;
+        Order order = orderRepository.findById(orderId);
+        if (order != null){
+            Order newOrder = new Order(order.getId(), order.getProducts(), order.getOrderTime(), order.getAuthor(), status);
+            orderRepository.save(newOrder);
+            return newOrder;
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 
     @Override
     public List<Order> findAllByAuthor(String author){
-        return null;
+        return orderRepository.findAllByAuthor(author);
     }
 
     @Override
     public Order findById(String orderId){
-        return null;
+        return orderRepository.findById(orderId);
     }
 
 }
