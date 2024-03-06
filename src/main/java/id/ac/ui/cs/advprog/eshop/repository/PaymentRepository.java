e67af6770162413812001dc49069327197d780f2
Athar Adista <athar0165@gmail.com>
2024-03-06 19:09:35 +0700

    add @Repository in PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,10 +1,13 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class PaymentRepository {
 
     private List<Payment> paymentData = new ArrayList<>();
