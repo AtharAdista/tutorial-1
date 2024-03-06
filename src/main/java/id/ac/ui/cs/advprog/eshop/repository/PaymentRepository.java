bfb8dddc5ee83dbbde8740bc7fb5bd8e2c02fe84
Athar Adista <athar0165@gmail.com>
2024-03-06 07:23:27 +0700

    [RED] Add PaymentRepository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaymentRepository {
+
+    private List<Payment> paymentData = new ArrayList<>();
+    public Payment save(Payment payment) {
+        return null;
+    }
+
+    public Payment findById (String id){
+        return null;
+    }
+
+    public List<Payment> findAllByMethod (String method){
+        return null;
+    }
+}
